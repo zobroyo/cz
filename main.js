7233b0af33cd5531d6aa2cd938ee4757e8c4d77b
zobroyo <94797038+zobroyo@users.noreply.github.com>
2023-11-30 21:24:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function add()
+{
+  var result =
+Number(document.getElementById("firstno").value)+
+Number(document.getElementById("secondno").value);
+  console.log(result)
+}
+
+function subtract()
+{
+  var result =
+Number(document.getElementById("firstno").value)-
+Number(document.getElementById("secondno").value);
+  console.log(result)
+}
+
+function divide()
+{
+  var result =
+Number(document.getElementById("firstno").value)/
+Number(document.getElementById("secondno").value);
+  console.log(result)
+}
+
+function multiply()
+{
+  var result =
+Number(document.getElementById("firstno").value)*
+Number(document.getElementById("secondno").value);
+  console.log(result)
+}
